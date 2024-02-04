e798048bddab3cc5eb6be34a60048f252d159949
DanielleGreiner <dmgreiner4@icloud.com>
2024-02-04 18:24:18 -0500

    Add SQL connector

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+
+public class Main {
+
+	private static final String uname = "service_acct";
+	private static final String pass = "aiosfhdifj";
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Gather info from user");
+
+System.out.println("Insert into database");
+
+System.out.println("Connect to DB with " + uname + ":" + pass);
+	}
+
+}
